a5fc85fa93200aa4b37a1ec84d07abc5fe9a97ae
20211TIIMI0044 <eduardo.olmosantana@gmail.com>
2023-03-24 11:10:59 -0300

    Parei no passo 7

---
+++ app/src/main/java/olmo/eduardo/galeria/activity/NewItemActivity.java
@@ -0,0 +1,16 @@
+package olmo.eduardo.galeria.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import olmo.eduardo.galeria.R;
+
+public class NewItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_item);
+    }
+}
