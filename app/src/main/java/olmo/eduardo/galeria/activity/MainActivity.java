e67244bb0dd48f42f9a4bc530d5ed5acd5b453cd
20211TIIMI0044 <eduardo.olmosantana@gmail.com>
2023-03-29 13:00:43 -0300

    Parei no tópico 8

--- app/src/main/java/olmo/eduardo/galeria/activity/MainActivity.java
+++ app/src/main/java/olmo/eduardo/galeria/activity/MainActivity.java
@@ -2,15 +2,36 @@ package olmo.eduardo.galeria.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import olmo.eduardo.galeria.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    static int NEW_ITEM_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fabAddItem = findViewById(R.id.fabAddNewItem);
+        fabAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(MainActivity.this,NewItemActivity.class);
+
+                startActivityForResult(i, NEW_ITEM_REQUEST);
+
+            }
+        });
+
+
+
     }
 }
