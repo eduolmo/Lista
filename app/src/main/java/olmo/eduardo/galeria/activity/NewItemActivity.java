b0ce8d4a91f9a4590cec09c83c04b4996914e377
20211TIIMI0044 <eduardo.olmosantana@gmail.com>
2023-03-31 11:12:41 -0300

    Terminei o tópico 11

--- app/src/main/java/olmo/eduardo/galeria/activity/NewItemActivity.java
+++ app/src/main/java/olmo/eduardo/galeria/activity/NewItemActivity.java
@@ -1,16 +1,102 @@
 package olmo.eduardo.galeria.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import olmo.eduardo.galeria.R;
+import olmo.eduardo.galeria.model.MyItem;
 
 public class NewItemActivity extends AppCompatActivity {
+
+    static int PHOTO_PICKER_REQUEST = 1;
+    Uri photoSelected = null;
+
+    static int NEW_ITEM_REQUEST = 1;
+    List<MyItem> itens = new ArrayList<>();
+
+    @Override
+    protected void onActivityResult(int resquestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(resquestCode,resultCode,data);
+        if(resquestCode == PHOTO_PICKER_REQUEST){
+            if(resultCode == Activity.RESULT_OK){
+                photoSelected = data.getData();
+                ImageView imvfotoPreview = findViewById(R.id.imvPhotoPreview);
+                imvfotoPreview.setImageURI(photoSelected);
+
+                MyItem myItem = new MyItem();
+                myItem.title = data.getStringExtra("title");
+                myItem.description = data.getStringExtra("description");
+                myItem.photo = data.getData();
+                itens.add(myItem);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
+
+        ImageButton imgCI = findViewById(R.id.imbCI);
+        imgCI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent,PHOTO_PICKER_REQUEST);
+
+            }
+        });
+
+        Button btnAddItem = findViewById(R.id.btnAddItem);
+        btnAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(photoSelected == null){
+                    Toast.makeText(NewItemActivity.this, "É necessário selecionar uma imagem!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                EditText etTitle = findViewById(R.id.etTitle);
+                String title = etTitle.getText().toString();
+                if(title.isEmpty()){
+                    Toast.makeText(NewItemActivity.this, "É necessário inserir um título", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                EditText etDesc = findViewById(R.id.etDesc);
+                String description = etDesc.getText().toString();
+                if(description.isEmpty()){
+                    Toast.makeText(NewItemActivity.this, "É necessário inserir uma descrição", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Intent i = new Intent();
+                i.setData(photoSelected);
+                i.putExtra("title",title);
+                i.putExtra("description",description);
+                setResult(Activity.RESULT_OK, i);
+                finish();
+
+            }
+        });
+
+
     }
 }
